9fbf825e01761022956236876ba99f2a38a6f6ee
DOM LEUNG <domleung@MacBook-Pro.local>
2023-11-25 22:14:38 -0800

    Update base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ const isProd = process.env.NODE_ENV === "production";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: "https://goghlab.github.io",
-  base: "/",
+  base: "/astro-launch-ui/",
+  src: 'src'
 });
