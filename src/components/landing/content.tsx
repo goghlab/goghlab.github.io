d62287dec6a444183c390b92e1c7ba0a255ed6eb
DOM LEUNG <domleung@MacBook-Pro.local>
2023-11-26 10:15:53 -0800

    resize img

--- src/components/landing/content.tsx
+++ src/components/landing/content.tsx
@@ -17,16 +17,16 @@ export function HeroSectionFour() {
 >
           <span className="absolute top-0 left-0 w-full h-full bg-center bg-cover  "></span>
           <Typography
-            variant="h1"
-            color="white"
+            variant="h3"
+            color="Black"
             className="mb-4 !leading-tight lg:text-7xl z-20"
           >
             Autonomous Retail
           </Typography>
           <Typography
             variant="lead"
-            color="white"
-            className="mb-12 w-full opacity-80 md:w-10/12 lg:w-7/12 mx-auto px-28 z-10"
+            color="Black"
+            className="mb-12 w-full opacity-50 md:w-10/12 lg:w-7/12 mx-auto px-28 z-10"
           >
             We&apos;re constantly trying to e
           </Typography>
