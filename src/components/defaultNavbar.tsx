cc6dfd0c53cfd069e1a4c971bb7b66beccccd9e7
DOM LEUNG <domleung@MacBook-Pro.local>
2023-11-26 09:52:44 -0800

    eilogo

--- src/components/defaultNavbar.tsx
+++ src/components/defaultNavbar.tsx
@@ -215,7 +215,7 @@ export default function Example() {
       <Navbar className="absolute mx-auto left-0 right-0 top-3 max-w-screen-xl px-4 py-2 z-10">
         <div className="flex items-center justify-between text-black">
         <img
-          src="public/eilogo.png"
+          src="eilogo.png"
           alt="Logo"
           className="mr-4 cursor-pointer py-1.5 lg:ml-2"
           width="140"
