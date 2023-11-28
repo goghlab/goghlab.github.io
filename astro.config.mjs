3268c0072bfed26f69bc72cd3d5d8ec11c3171e8
DOM LEUNG <domleung@MacBook-Pro.local>
2023-11-28 03:05:55 -0800

    remove offercard

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://goghlab.github.io",
   base: "/",
   src: 'src',
 
