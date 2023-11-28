fc081099c52693801846f77542f6cac361c88580
DOM LEUNG <domleung@MacBook-Pro.local>
2023-11-27 21:42:44 -0800

    updated script

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+// Get the element by tag name
+var bodyElement = document.querySelector('body');
 // Get the element by ID
 var offerCard = document.getElementById('offerCard');
 
-// Check if the element exists before modifying it
-if (offerCard) {
-  // Remove the 'active' class
-  offerCard.classList.remove('active');
-
-  // Disable other actions or modify the element further as needed
-  // For example, you can set the element to be hidden
-  // offerCard.style.display = 'none';
+// Check if the body element has the 'overflow-x-hidden' class
+if (bodyElement && bodyElement.classList.contains('overflow-x-hidden')) {
+  // Check if the offerCard element exists before modifying it
+  if (offerCard) {
+    // Remove the 'active' class to hide the offerCard
+    offerCard.classList.remove('active');
+  }
 }
-
