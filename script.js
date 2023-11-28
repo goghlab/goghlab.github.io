4b86d11280a4c37ec3345617d1102e9d6dd02fc8
DOM LEUNG <domleung@MacBook-Pro.local>
2023-11-27 19:48:35 -0800

    disable offercard

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Get the element by tag name
+var bodyElement = document.querySelector('body');
+
+// Check if the element exists before modifying it
+if (bodyElement) {
+  // Remove the 'overflow-x-hidden' class
+  bodyElement.classList.remove('overflow-x-hidden');
+}
